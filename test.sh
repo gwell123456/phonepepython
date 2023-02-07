59caddbc92de1af020b8e186617996ba8376e3e7
gwell123456 <124039543+gwell123456@users.noreply.github.com>
2023-02-07 17:19:14 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This is a Test Repository"
+
+API="https://api.phonepetest.com:8080"
+API_KEY="AZHJSGSGC88767986HSGS"
+API_SECRET="JSDHSBNSJH6hbbhshbUHBShbshs9h77s8"
+
+echo $API_KEY
+echo $API_SECRET
